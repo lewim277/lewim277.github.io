089d3852a45c5cf4e334e75217e8fc7e52895bd9
lewim277 <lewim277@newschool.edu>
2015-12-04 16:22:02 +0000

    wow

--- scriptplay.js
+++ scriptplay.js
@@ -15,9 +15,52 @@ $(".lever2").click(function() {
   $(".lever2").hide();
 });
 
+$(".lever3").click(function() {
+    console.log("off");
+  $(".lever2").show();
+  $(".lever8").hide();
+});
+
 $(".lever4").click(function() {
     console.log("off1");
     $(".eye2").show();
     $(".lever4").hide();
     $(".eye").hide();
+});
+
+$(".lever5").click(function() {
+    console.log("off1");
+    $(".eye3").show();
+    $(".lever5").hide();
+    $(".eye2").hide();
+});
+
+$(".lever6").click(function() {
+    console.log("off1");
+    $(".lever5").show();
+    $(".lever6").hide();
+});
+
+$(".lever7").click(function() {
+    console.log("off1");
+    $(".lever6").show();
+    $(".lever2").hide();
+});
+
+$(".lever8").click(function() {
+    console.log("off1");
+    $(".lever10").hide();
+    $(".leve8").hide();
+});
+
+$(".lever1").click(function() {
+    console.log("off1");
+    $(".lever9").hide();
+    $(".leve1").hide();
+});
+
+$(".lever10").click(function() {
+    console.log("off1");
+    $(".lever10").hide();
+    $(".leve9").show();
 });
